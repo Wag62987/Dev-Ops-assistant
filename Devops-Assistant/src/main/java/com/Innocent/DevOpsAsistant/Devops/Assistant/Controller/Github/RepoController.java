e24d2c16e7c88bfa442bbb3c31975ef5e79c6fc1
Mayur Waghade <mayurwaghade672@gmail.com>
2026-03-30 10:19:19 +0530

    Update CORS origin for RepoController

--- Devops-Assistant/src/main/java/com/Innocent/DevOpsAsistant/Devops/Assistant/Controller/Github/RepoController.java
+++ Devops-Assistant/src/main/java/com/Innocent/DevOpsAsistant/Devops/Assistant/Controller/Github/RepoController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://operadev.netlify.app")
 @Slf4j
 @RestController
 @RequestMapping("/repos")
